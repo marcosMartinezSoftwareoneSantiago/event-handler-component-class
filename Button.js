01687ebca6244103ec640b6bd46e486d1ff0a79a
marcosMartinezSoftwareoneSantiago <Marcos.Martinez@Softwareone.com>
2022-07-22 11:45:09 +0200

    handleevent-onevent-props-event

--- Button.js
+++ Button.js
@@ -2,6 +2,6 @@ import React from "react";
 
 export class Button extends React.Component {
   render() {
-    return <button onClick={this.props.talk}>Click me!</button>;
+    return <button onClick={this.props.onClick}>Click me!</button>;
   }
 }
